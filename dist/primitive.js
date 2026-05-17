08f094059b718711fce1dcf765f2b6321c1f4de1
Shihab <mohammadshihabuddin7@gmail.com>
2026-05-17 23:53:46 +0600

    TS LEARING DAY 1

---
+++ dist/primitive.js
@@ -0,0 +1,17 @@
+"use strict";
+//JS  - TS
+// string,number,boolean,undefined,null
+Object.defineProperty(exports, "__esModule", { value: true });
+//ts: never,unknown,void
+let userName = "Shihab";
+userName = "shihab123";
+let userId = 123;
+// userId ="123"
+userId.toFixed();
+let isAdmin = false;
+isAdmin = true;
+let x = undefined;
+x = 6;
+x = 'five';
+console.log(x);
+//# sourceMappingURL=primitive.js.map
---
+++ src/primitive.ts
@@ -0,0 +1,25 @@
+//JS  - TS
+// string,number,boolean,undefined,null
+
+//ts: never,unknown,void
+
+let userName : string = "Shihab";
+
+userName = "shihab123";
+
+let userId = 123
+
+// userId ="123"
+
+userId.toFixed();
+
+let isAdmin = false;
+
+isAdmin = true;
+
+let x  = undefined;
+
+x = 6;
+x= 'five'
+
+console.log(x);
